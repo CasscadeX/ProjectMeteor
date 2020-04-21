47423d50ca5b2fe9e1cbc774cd96ffc689fc7333
Prashubh Atri <prashubh27@gmail.com>
2020-04-21 13:33:52 +0530

    restarted

--- routes/api/auth.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express')
-
-const route = Router()
-
-route.get('/',(req,res)=>{
-    res.send('Auth Route')
-})
-
-module.exports = { route }
--- routes/api/profiles.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express')
-
-const route = Router()
-
-route.get('/',(req,res)=>{
-    res.send('Profile Route')
-})
-
-module.exports = { route }
