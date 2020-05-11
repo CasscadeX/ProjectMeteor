d6f61ba4896fa012d365ccabbe2ddb18cfe64aec
Prashubh Atri <prashubh27@gmail.com>
2020-05-11 15:05:12 +0530

    token error

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const connectDB = require('./data/db')
 const apiRoute = require('./routes/api').route
+const path = require('path');
 
 const app = express()
 
@@ -13,6 +14,14 @@ app.use(express.urlencoded({extended: true}))
 
 //routes
 app.use('/api',apiRoute)
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 //assigning the PORT
 const PORT = process.env.PORT || 5555;
